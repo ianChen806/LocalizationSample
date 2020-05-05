7744b1fb6182de53e09126dc6d4458117f8772b8
IanChen <ian.chen@taiwantaxi.com.tw>
2020-05-05 15:57:47 +0800

    add resource file, and adjust startup.

--- LocalizationSample/Startup.cs
+++ LocalizationSample/Startup.cs
@@ -27,7 +27,10 @@ namespace LocalizationSample
         {
             services.AddControllersWithViews();
 
-            services.AddLocalization();
+            services.AddLocalization(options =>
+            {
+                options.ResourcesPath = "Resources";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
